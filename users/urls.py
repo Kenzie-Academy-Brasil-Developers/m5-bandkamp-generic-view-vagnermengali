652d4c27350cb1106af5511399642ceb59f9cbcf
vagnermengali <vagmengali2015@gmail.com>
2022-12-12 22:21:31 -0300

    feat: adding complete T2

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from . import views
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path("users/", views.UserView.as_view()),
-    path("users/<int:pk>/", views.UserDetailView.as_view()),
+    path("users/", views.CreateAPIView.as_view()),
+    path("users/<int:pk>/", views.RetrieveUpdateDestroyAPIView.as_view()),
     path("users/login/", jwt_views.TokenObtainPairView.as_view()),
 ]
