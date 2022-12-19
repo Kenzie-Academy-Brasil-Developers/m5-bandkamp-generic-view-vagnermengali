f983a85c81b4f09cb1292df090cb77e79f6ad2ae
vagnermengali <vagmengali2015@gmail.com>
2022-12-19 10:50:28 -0300

    feat: adding complete T4

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("users/", views.CreateAPIView.as_view()),
     path("users/<int:pk>/", views.RetrieveUpdateDestroyAPIView.as_view()),
     path("users/login/", jwt_views.TokenObtainPairView.as_view()),
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('docs/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
